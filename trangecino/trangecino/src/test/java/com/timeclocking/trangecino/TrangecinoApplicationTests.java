6edef9976721d494bd4c0d190fad503232f96ff7
Nic0Joos <72554161+Nic0Joos@users.noreply.github.com>
2021-03-30 14:25:40 +0200

    Spring Initializr files

---
+++ trangecino/trangecino/src/test/java/com/timeclocking/trangecino/TrangecinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeclocking.trangecino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrangecinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
