e45eb92d5609517ce4405ea59e7b03683048ae87
lucaw <luca.weisskopf@students.fhnw.ch>
2021-04-06 16:06:36 +0200

    employee service

--- trangecino/trangecino/src/main/java/com/time/trangecino/business/service/EmployeeService.java
+++ trangecino/trangecino/src/main/java/com/time/trangecino/business/service/EmployeeService.java
@@ -2,4 +2,18 @@ package com.time.trangecino.business.service;
 
 public class EmployeeService {
 
+    public void clockTime () {
+    }
+
+    public void editTime () {
+    }
+
+    public void deleteTime () {
+    }
+
+    public void viewTime () {
+    }
+
+    public void addHoliday () {
+    }
 }
