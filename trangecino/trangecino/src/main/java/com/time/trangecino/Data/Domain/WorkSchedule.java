9a61d987c7a07e1567bf3c77b86c3c1ece4b4050
kaan2302 <kaanyildiz1402@gmail.com>
2021-04-06 15:40:31 +0200

    Simple data types //All data types added

--- trangecino/trangecino/src/main/java/com/time/trangecino/Data/Domain/WorkSchedule.java
+++ trangecino/trangecino/src/main/java/com/time/trangecino/Data/Domain/WorkSchedule.java
@@ -1,11 +1,18 @@
 package com.time.trangecino.Data.Domain;
 
 
+import java.sql.Time;
+import java.util.Date;
+
 //written by Kaan
-public class WorkSchedule{
+public class WorkSchedule {
 
 
     private int ID;
     private Date date;
-
+    private boolean Holiday;
+    private Time StartMorning;
+    private Time Lunch;
+    private Time StartAfternoon;
+    private Time EndDay;
 }
