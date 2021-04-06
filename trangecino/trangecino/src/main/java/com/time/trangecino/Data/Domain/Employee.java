d1d3bc341324000ea5ee24dfd0fbc7e87f50a60c
lucaw <luca.weisskopf@students.fhnw.ch>
2021-04-06 15:28:51 +0200

    employee class

---
+++ trangecino/trangecino/src/main/java/com/time/trangecino/Data/Domain/Employee.java
@@ -0,0 +1,24 @@
+package com.time.trangecino.Data.Domain;
+
+//written by Luca
+public class Employee extends User {
+
+    public class clockTime() {
+    }
+
+    public class editTime() {
+    }
+
+    public class deleteTime() {
+    }
+
+    public class viewTime (){
+    }
+
+    public class addHoliday() {
+    }
+
+    public int getTime () {return clockTime
+    }
+
+}
--- trangecino/trangecino/src/main/java/com/time/trangecino/Data/Domain/User.java
+++ trangecino/trangecino/src/main/java/com/time/trangecino/Data/Domain/User.java
@@ -11,9 +11,7 @@ public abstract class User {
     private String name;
     private String familyname;
 
-    public int getID() {
-        return ID;
-    }
+    public int getID() { return ID; }
 
     public void setID(int ID) {
         this.ID = ID;
