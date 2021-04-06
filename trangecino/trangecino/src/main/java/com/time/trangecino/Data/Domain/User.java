2098f65a1c832dad2634d0709f4114370fb646e0
joosn <nico.joos@students.fhnw.ch>
2021-04-06 15:06:36 +0200

    Abstract User class

---
+++ trangecino/trangecino/src/main/java/com/time/trangecino/Data/Domain/User.java
@@ -0,0 +1,53 @@
+package com.time.trangecino.Data.Domain;
+
+
+
+public abstract class User {
+
+
+    private int ID;
+    private String email;
+    private String password;
+    private String name;
+    private String familyname;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return null;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamilyname() {
+        return familyname;
+    }
+
+    public void setFamilyname(String familyname) {
+        this.familyname = familyname;
+    }
+}
