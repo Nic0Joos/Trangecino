8ab772a4c6beb3d18fdb31f062978779340de378
lucaw <luca.weisskopf@students.fhnw.ch>
2021-04-06 15:56:16 +0200

    updated employee

--- trangecino/trangecino/src/main/java/com/time/trangecino/Data/Domain/Employee.java
+++ trangecino/trangecino/src/main/java/com/time/trangecino/Data/Domain/Employee.java
@@ -3,18 +3,4 @@ package com.time.trangecino.Data.Domain;
 //written by Luca
 public class Employee extends User {
 
-    public clockTime() {
-    }
-
-    public editTime() {
-    }
-
-    public deleteTime() {
-    }
-
-    public viewTime (){
-    }
-
-    public addHoliday() {
-    }
 }
