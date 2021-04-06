c5dd8923278bdb78d0c31eeb90637fdd8dec13af
Nic0Joos <72554161+Nic0Joos@users.noreply.github.com>
2021-04-06 14:59:05 +0200

    Add files via upload

---
+++ trangecino/trangecino/src/main/java/com/time/trangecino/TrangecinoApplication.java
@@ -0,0 +1,13 @@
+package com.time.trangecino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrangecinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrangecinoApplication.class, args);
+	}
+
+}
---
+++ trangecino/trangecino/src/test/java/com/time/trangecino/TrangecinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time.trangecino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrangecinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
