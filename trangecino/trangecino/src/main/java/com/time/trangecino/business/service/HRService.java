3f834c5728eec9805240bc64e2a530eb573a591f
Alexjames21 <alex.herzog@students.fhnw.ch>
2021-04-15 15:24:10 +0200

    Changed workday in workschedule

--- trangecino/trangecino/src/main/java/com/time/trangecino/business/service/HRService.java
+++ trangecino/trangecino/src/main/java/com/time/trangecino/business/service/HRService.java
@@ -1,12 +1,16 @@
 package com.time.trangecino.business.service;
 
+import java.sql.Time;
+import java.util.Date;
+
 //Written by Alex
 public class HRService {
 	
 	public void assignProject () {
 	}
 	
-	public void createWorkday () {
+	public void createWorkschedule() {
+		
 	}
 	
 
