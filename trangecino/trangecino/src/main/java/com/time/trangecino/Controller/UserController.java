370281ce4e66f45673d93119bae0e0048bdb9594
lucaw <luca.weisskopf@students.fhnw.ch>
2021-04-27 15:56:23 +0200

    UserController login

---
+++ trangecino/trangecino/src/main/java/com/time/trangecino/Controller/UserController.java
@@ -0,0 +1,19 @@
+package com.time.trangecino.Controller;
+
+import com.time.trangecino.Data.Domain.Employee;
+import com.time.trangecino.business.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private EmployeeService employeeService
+
+    @GetMapping("/login")
+    public String getLoginView() {
+        return "Login.html";
+    }
+}
