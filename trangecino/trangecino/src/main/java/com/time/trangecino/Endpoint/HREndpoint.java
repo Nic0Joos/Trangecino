f4a0a9773e0946931146d3bcc1aa3c2b0cc040fe
Alexjames21 <alex.herzog@students.fhnw.ch>
2021-04-27 14:50:40 +0200

    HR Endpoint changes

--- trangecino/trangecino/src/main/java/com/time/trangecino/Endpoint/HREndpoint.java
+++ trangecino/trangecino/src/main/java/com/time/trangecino/Endpoint/HREndpoint.java
@@ -1,4 +1,16 @@
 package com.time.trangecino.Endpoint;
 
+import com.time.trangecino.business.service.EmployeeService;
+import com.time.trangecino.business.service.HRService;
+import org.springframework.beans.factory.annotation.Autowired;
+//Written by Alex
 public class HREndpoint {
+
+    private HREndpoint HREndpoint;
+
+    @Autowired
+    public HREndpoint(HREndpoint HREndpoint) {
+        this.HREndpoint = HREndpoint;
+    }
+
 }
