a81dea0326f9f9931a926b495f367f2c8c386834
kaan2302 <kaanyildiz1402@gmail.com>
2021-04-27 14:45:17 +0200

    //Endpoint ordner was at the wrong place

--- trangecino/trangecino/src/main/java/com/time/trangecino/Endpoint/EmployeeEndpoint.java
+++ trangecino/trangecino/src/main/java/com/time/trangecino/Endpoint/EmployeeEndpoint.java
@@ -1,8 +1,7 @@
-package Endpoint;
+package com.time.trangecino.Endpoint;
 
 import com.time.trangecino.business.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
