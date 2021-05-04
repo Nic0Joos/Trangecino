259530229d73923f44dea650222a41185b25888f
Alexjames21 <alex.herzog@students.fhnw.ch>
2021-05-04 14:30:11 +0200

    Add HRRepository
    Some HR Service changes

---
+++ trangecino/trangecino/src/main/java/com/time/trangecino/Repository/HRRepository.java
@@ -0,0 +1,9 @@
+package com.time.trangecino.Repository;
+
+import com.time.trangecino.Data.Domain.HR;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HRRepository extends JpaRepository <HR,Long>{
+
+
+}
