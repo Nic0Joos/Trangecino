f7e8cc27739b445d7fdd47ad64fa32b527f942c7
lucaw <luca.weisskopf@students.fhnw.ch>
2021-05-04 15:45:49 +0200

    IndexController

--- trangecino/trangecino/src/main/java/com/time/trangecino/Controller/IndexController.java
+++ trangecino/trangecino/src/main/java/com/time/trangecino/Controller/IndexController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @GetMapping
     public String getIndexView(){
-        return "index.html";
+        return "Index.html";
     }
 }
