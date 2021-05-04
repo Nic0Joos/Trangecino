469d5100071e52f23e9c29fd16f1a6cc93735fb6
Alexjames21 <alex.herzog@students.fhnw.ch>
2021-05-04 14:37:08 +0200

    Work on HR Repo

--- trangecino/trangecino/src/main/java/com/time/trangecino/Repository/EmployeeRepository.java
+++ trangecino/trangecino/src/main/java/com/time/trangecino/Repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.time.trangecino.Repository;
 
 import com.time.trangecino.Data.Domain.Employee;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- trangecino/trangecino/src/main/java/com/time/trangecino/Repository/HRRepository.java
+++ trangecino/trangecino/src/main/java/com/time/trangecino/Repository/HRRepository.java
@@ -1,9 +1,15 @@
 package com.time.trangecino.Repository;
 
+import com.time.trangecino.Data.Domain.Employee;
 import com.time.trangecino.Data.Domain.HR;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface HRRepository extends JpaRepository <HR,Long>{
 
-
+    @Query("select u from HRAdminEntity u where current_date >=u.startDate and current_date <=u.endDate and u.employeeEntity =:employee")
+    Optional<HR> findByEmployee(@Param("employee") Employee employee);
 }
