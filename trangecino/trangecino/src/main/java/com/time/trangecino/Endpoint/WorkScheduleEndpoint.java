778a81568c0569026eed5bd715066c04cd4bf96c
kaan2302 <kaanyildiz1402@gmail.com>
2021-05-05 12:14:16 +0200

    // REST Controller

--- trangecino/trangecino/src/main/java/com/time/trangecino/Endpoint/WorkScheduleEndpoint.java
+++ trangecino/trangecino/src/main/java/com/time/trangecino/Endpoint/WorkScheduleEndpoint.java
@@ -1,13 +1,22 @@
 package com.time.trangecino.Endpoint;
 
+import com.time.trangecino.Data.Domain.WorkSchedule;
+import com.time.trangecino.business.service.WorkScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-
-public class WorkScheduleEndpoint {
-
-@PostMapping
-public postWorkschedule()
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
 
-}
+
+    @RestController
+    @RequestMapping(path = "/api")
+    public class WorkScheduleEndpoint {
+        @Autowired
+        private WorkScheduleService workSService;
+        private Object WorkSchedule;}
+
