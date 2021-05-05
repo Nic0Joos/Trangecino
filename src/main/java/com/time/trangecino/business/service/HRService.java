7e7f460ac67239768bce6380df255ca26f7e3ca1
Alexjames21 <alex.herzog@students.fhnw.ch>
2021-05-05 14:07:58 +0200

    Service Changes

--- src/main/java/com/time/trangecino/business/service/HRService.java
+++ src/main/java/com/time/trangecino/business/service/HRService.java
@@ -3,9 +3,12 @@ package com.time.trangecino.business.service;
 import java.sql.Time;
 import java.time.DayOfWeek;
 import java.util.Date;
+
+import com.time.trangecino.Data.Domain.HR;
 import com.time.trangecino.Data.Domain.WorkSchedule;
 import com.time.trangecino.Repository.EmployeeRepository;
 import com.time.trangecino.Repository.HRRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,13 +23,15 @@ public class HRService {
 	@Autowired
 	EmployeeRepository employeeRepository;
 
-	public String createOverview() {
+
+	public String createOverview(){
 		return null;
 	}
 	
 	public void assignProject () {
+
+
 	}
-	
 	public void createWorkschedule () {
 	     
 	}
