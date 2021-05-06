c19b7013c079f404b293524231c2e9d5c0b9abd4
kaan2302 <kaanyildiz1402@gmail.com>
2021-05-06 16:45:49 +0200

    // started with the endpoint

--- src/main/java/com/time/trangecino/Endpoint/WorkScheduleEndpoint.java
+++ src/main/java/com/time/trangecino/Endpoint/WorkScheduleEndpoint.java
@@ -1,4 +1,23 @@
 package com.time.trangecino.Endpoint;
 
+import com.time.trangecino.business.service.WorkScheduleService;
+import com.time.trangecino.Data.Domain.Admin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+//written by Kaan
+
+@RestController
+@RequestMapping(path= "/WorkSchedule")
 public class WorkScheduleEndpoint {
+
+    private WorkScheduleEndpoint workSAPI;
+
+    @Autowired
+    public WorkScheduleEndpoint(AdminEndpoint adminEndpoint) {
+        this.workSAPI = workSAPI;
+    }
+
 }
+
+//Put Post Delete
