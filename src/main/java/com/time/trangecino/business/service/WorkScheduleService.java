687301aa05db470aaa9f7315da68dfb081e67c12
kaan2302 <kaanyildiz1402@gmail.com>
2021-05-11 15:42:00 +0200

    //boolean for business logic,

--- src/main/java/com/time/trangecino/business/service/WorkScheduleService.java
+++ src/main/java/com/time/trangecino/business/service/WorkScheduleService.java
@@ -1,27 +1,28 @@
 package com.time.trangecino.business.service;
 
 
-import java.time.DayOfWeek;
-
 //written by Kaan
 public class WorkScheduleService {
-/*
-    enum DaysOfWeek {
-   days of the week and the values which I gave them
-      MONDAY(1) {
-          public DayOfWeek next() { return MONDAY; }
-   }, TUESDAY(2) {
-          public DayOfWeek next() { return TUESDAY; }
-   }, WEDNESDAY(3) {
-          public DayOfWeek next() { return WEDNESDAY; }
-   }, THURSDAY(4) {
-          public DayOfWeek next() { return THURSDAY; }
-   }, FRIDAY(5) {
-          public DayOfWeek next() { return FRIDAY; }
-   }, SATURDAY(6) {
-            public DayOfWeek next() { return SATURDAY; }
-   }, SUNDAY(7) {
-            public DayOfWeek next() { return SUNDAY; }
 
-    }}*/
+      private WorkingDaysStatus status;
+
+public enum WorkingDaysStatus {
+  // possible workdays
+      WORKDAY,
+      HOLIDAY,
+      INJURY;
+}
+
+ /*public boolean areUWorking() {
+      if (getWorkingDaysStatus() == WorkingDaysStatus.WORKDAY) {
+            return true;
+          }
+          return false;
       }
+
+
+    private void getWorkingDaysStatus() {
+    }
+
+
+}
