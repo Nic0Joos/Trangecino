648dc8f601f0ebfa7ed2c68bf219c46c477c09de
Alexjames21 <alex.herzog@students.fhnw.ch>
2021-05-12 14:01:48 +0200

    Query

--- src/main/java/com/time/trangecino/Repository/HRRepository.java
+++ src/main/java/com/time/trangecino/Repository/HRRepository.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 
  //Written by Alex
 public interface HRRepository extends JpaRepository <HR,Long>{
-
     //@Query("select u from HR u where current_date >=u.startDate and current_date <=u.endDate and u.Employee =:employee")
     //Optional<HR> findByEmployee(@Param("employee") Employee employee);
 }
