f24c59b6c2c5eda0d1d39df29421fb135aa84b66
Alexjames21 <alex.herzog@students.fhnw.ch>
2021-05-12 14:17:03 +0200

    HR Repo find by email

--- src/main/java/com/time/trangecino/Repository/HRRepository.java
+++ src/main/java/com/time/trangecino/Repository/HRRepository.java
@@ -5,12 +5,16 @@ import com.time.trangecino.Data.Domain.HR;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 
  //Written by Alex
-public interface HRRepository extends JpaRepository <HR,Long>{
+@Repository
+public interface HRRepository extends JpaRepository<HR, Long> {
+
+     HR findByEmail(String email);
     //@Query("select u from HR u where current_date >=u.startDate and current_date <=u.endDate and u.Employee =:employee")
     //Optional<HR> findByEmployee(@Param("employee") Employee employee);
 }
