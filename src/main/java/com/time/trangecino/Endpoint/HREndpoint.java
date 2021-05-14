6b5e26c60ec2b19ccbb0fe3742ec65b1163c4cc0
Alexjames21 <alex.herzog@students.fhnw.ch>
2021-05-14 15:01:11 +0200

    Register HR

--- src/main/java/com/time/trangecino/Endpoint/HREndpoint.java
+++ src/main/java/com/time/trangecino/Endpoint/HREndpoint.java
@@ -5,23 +5,28 @@ import com.time.trangecino.Data.Domain.HR;
 import com.time.trangecino.business.service.EmployeeService;
 import com.time.trangecino.business.service.HRService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //Written by Alex
 
 @RestController
-@RequestMapping(path= "/HR")
+@RequestMapping(path= "/hr")
 public class HREndpoint {
 
-    private HREndpoint hrEndpoint;
+    private HRService hrService;
 
     @Autowired
-    public HREndpoint(HREndpoint hrEndpoint) {
-        this.hrEndpoint = hrEndpoint;
-    }
+    public HREndpoint(HRService hrService) { this.hrService = hrService; }
 
-    //@PutMapping
-    //Assign project from service
+    @PostMapping
+    public void postHR(/*@RequestBody?*/ HR hr) {
+        try {
+            hrService.registerHR(hr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/com/time/trangecino/business/service/HRService.java
+++ src/main/java/com/time/trangecino/business/service/HRService.java
@@ -1,11 +1,14 @@
 package com.time.trangecino.business.service;
 
+import com.time.trangecino.Data.Domain.Admin;
 import com.time.trangecino.Data.Domain.HR;
 import com.time.trangecino.Repository.EmployeeRepository;
 import com.time.trangecino.Repository.HRRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
 
 //Written by Alex
 
@@ -15,25 +18,33 @@ public class HRService {
 	@Autowired
 	HR User;
 
-	@Autowired
-	HRRepository HR;
-
 	public String getCurrentHRUser() {
 		String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		return User.getEmail();
 	}
 
 
+	@Autowired
+	HRRepository hRRepository;
+
+	public void registerHR(@Validated HR hr) throws Exception {
+		if (hr.getID() == null) {
+			if (hRRepository.findByEmail(hr.getEmail()) != null) {
+				throw new Exception("Email address " + hr.getEmail() + "already assigned to another HR Employee.");
+			} else {
+				hRRepository.save(hr);
+			}
+		}
 	/* public String createOverview(){
 		return null;
 	}
-	
-	public void assignProject () {
 
+	public void assignProject () {
+*/
 
 	}
-	public void createWorkschedule () {
-	     
+	/*public void createWorkschedule extends WorkScheduleService {
+
 	}
 */
 }
