93a62c4c36d4104b5b56dd54466169d71b028ffc
kaan2302 <kaanyildiz1402@gmail.com>
2021-05-18 14:20:28 +0200

    // ausgeklammert

--- src/main/java/com/time/trangecino/Endpoint/WorkScheduleEndpoint.java
+++ src/main/java/com/time/trangecino/Endpoint/WorkScheduleEndpoint.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 //written by Kaan
-
+/*
 @RestController
 @RequestMapping(path= "/WorkSchedule")
 public class WorkScheduleEndpoint {
--- src/main/java/com/time/trangecino/business/service/WorkScheduleService.java
+++ src/main/java/com/time/trangecino/business/service/WorkScheduleService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 //written by Kaan
+/*
 @Service
+ */
 
 public class WorkScheduleService {
 
