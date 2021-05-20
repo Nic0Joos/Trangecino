f558dad96b877aa0fb20e818e3ed7ce9c2f01084
kaan2302 <kaanyildiz1402@gmail.com>
2021-05-20 19:27:02 +0200

    // some fixes in the repository

--- src/main/java/com/time/trangecino/Repository/WorkScheduleRepository.java
+++ src/main/java/com/time/trangecino/Repository/WorkScheduleRepository.java
@@ -5,7 +5,7 @@ import com.time.trangecino.Data.Domain.WorkSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/*
+
 //written by Kaan
 @Repository
 public interface WorkScheduleRepository extends JpaRepository<WorkSchedule, Boolean> {
