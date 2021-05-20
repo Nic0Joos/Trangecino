0d769c0297e60af94a4e55a34de633c33be365b1
kaan2302 <kaanyildiz1402@gmail.com>
2021-05-20 19:26:08 +0200

    // some fixes

--- src/main/java/com/time/trangecino/Repository/WorkScheduleRepository.java
+++ src/main/java/com/time/trangecino/Repository/WorkScheduleRepository.java
@@ -1,11 +1,15 @@
 package com.time.trangecino.Repository;
 
+import com.time.trangecino.Data.Domain.Employee;
+import com.time.trangecino.Data.Domain.WorkSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
+/*
 //written by Kaan
 @Repository
-public interface WorkScheduleRepository extends JpaRepository {
+public interface WorkScheduleRepository extends JpaRepository<WorkSchedule, Boolean> {
+    WorkSchedule findByboolean(boolean Holiday);
+    void deleteById(Integer ID);
 
 }
--- src/main/java/com/time/trangecino/business/service/WorkScheduleService.java
+++ src/main/java/com/time/trangecino/business/service/WorkScheduleService.java
@@ -1,15 +1,18 @@
 package com.time.trangecino.business.service;
 
-
+/*
+import com.time.trangecino.Data.Domain.Employee;
+import com.time.trangecino.Data.Domain.WorkSchedule;
 import com.time.trangecino.Repository.EmployeeRepository;
 import com.time.trangecino.Repository.WorkScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Valid;
+
 //written by Kaan
 /*
 @Service
- */
 
 public class WorkScheduleService {
 
@@ -18,17 +21,25 @@ public class WorkScheduleService {
 
       private WorkingDaysStatus status;
 
+    public void createWorkschedule(@Valid WorkSchedule workschedule) throws Exception {
+        if (workschedule.getWorkSchedule == "Holiday") {
+            if (WorkScheduleRepository.findByHoliday(workschedule.getWorkschedule()) != null) {
+                throw new Exception("Please create a Workschedule");
+            }
+        }
+
+
 
       public enum WorkingDaysStatus {
   // possible workdays
-      WORKDAY,
-      HOLIDAY,
-      ILLNESS;
+      OutOfOffice,
+      InOffice,
+      ;
 }}
 
- /*
+
  public boolean areUWorking() {
-      if (getWorkingDaysStatus() == WorkingDaysStatus.WORKDAY) {
+      if (getWorkingDaysStatus() == WorkingDaysStatus.InOffice) {
             return true;
           }
           return false;
@@ -38,5 +49,20 @@ public class WorkScheduleService {
     private void getWorkingDaysStatus() {
     }
 
+ public void createWorkschedule(@Valid Workschedule workschedule) throws Exception {
+        if (WorkSchedule.getID() == null) {
+            if (WorkScheduleRepository.findByEmail(Workschedule.getEmail()) != null) {
+                throw new Exception("Please enter a new email address");
+            }
+        }
+        employee.setPassword(passwordEncoder.encode(employee.getPassword()));
+        employeeRepository.save(employee);
+    }
 
-}*/
+
+
+
+
+
+}
+*/
