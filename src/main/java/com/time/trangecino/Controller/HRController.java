743e7730a630c34854698e84d70c571b8c9514e8
Alexjames21 <alex.herzog@students.fhnw.ch>
2021-05-21 10:51:22 +0200

    HR Controller

--- src/main/java/com/time/trangecino/Controller/HRController.java
+++ src/main/java/com/time/trangecino/Controller/HRController.java
@@ -1,6 +1,7 @@
 package com.time.trangecino.Controller;
 
 import com.time.trangecino.Data.Domain.HR;
+import com.time.trangecino.business.service.EmployeeService;
 import com.time.trangecino.business.service.HRService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,35 +14,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/HR")
 public class HRController {
 
-   private HRService hrService;
+    @Autowired
+    private HRService hrService;
 
-   @Autowired
-    public HRController(HRService hrService) {
-        this.hrService = hrService;
+    @GetMapping
+    public String getLoginView() {
+        return "HRPanel.html";
     }
-
-  /*  @PostMapping
-    public void registerHR(@RequestBody HR hr) {
-      return HRService.registerHR(hr);
-    }
-*/
 }
-
-
-
-
-
-/*  @GetMapping
-    public String getHRView(Model model) {
-        try {
-            if (HRService.getCurrentHRUser().isHR())
-                return "HR";
-            else
-                return "dashboard";
-        } catch (Exception e) {
-            return "login";
-    }
-
-  //  public ResponseEntity<CustomMessageWithId> addHradmin(@RequestBody HR request) {
-    //    return hrService.addHR(request);
-    //} */
