7e263c1bf0081b95a01670da4e04e8a5d9a57060
lucaw <luca.weisskopf@students.fhnw.ch>
2021-05-22 22:57:20 +0200

    SecurityConfig and LoginError.html added, implemented necessary code for login

--- src/main/java/com/time/trangecino/Config/SecurityConfig.java
+++ src/main/java/com/time/trangecino/Config/SecurityConfig.java
@@ -2,12 +2,44 @@ package com.time.trangecino.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    //employee, HR and Admin manually created with username and password.
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("employee1").password(passwordEncoder().encode("employee1Pass"))
+                .and()
+                .withUser("HR1").password(passwordEncoder().encode("HR2Pass"))
+                .and()
+                .withUser("admin1").password(passwordEncoder().encode("adminPass"));
+    }
+
+    //authorizing requests, redirecting to html pages
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/Login.html")
+                .failureUrl("/LoginError.html")
+                .and()
+                .logout();
+    }
+
+        //password encoder, in that case the BCryptPassword Encoder
         @Bean
         public PasswordEncoder passwordEncoder() {
             return new BCryptPasswordEncoder();
