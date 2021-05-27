1d166236ca0fb9dd270e1d5f702441698be2c0c4
lucaw <luca.weisskopf@students.fhnw.ch>
2021-05-27 10:48:51 +0200

    testing

--- src/main/java/com/time/trangecino/Config/SecurityConfig.java
+++ src/main/java/com/time/trangecino/Config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(final HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/Index.html", "/assets/**").permitAll()
+                    .antMatchers("/", "/Index.html", "/Team.html", "/assets/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
