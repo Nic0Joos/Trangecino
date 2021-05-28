ce6b5ae0669eb18dbd40d9c9c6763034bf490379
joosn <nico.joos@students.fhnw.ch>
2021-05-28 08:28:15 +0200

    bugfix

--- src/main/java/com/time/trangecino/Controller/AdminController.java
+++ src/main/java/com/time/trangecino/Controller/AdminController.java
@@ -1,7 +1,5 @@
 package com.time.trangecino.Controller;
 
-import com.time.trangecino.business.service.AdminService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(path = "/Admin")
 public class AdminController {
 
-    @Autowired
-    AdminService adminService;
 
     @GetMapping
     public String getAdminView(){
--- src/main/java/com/time/trangecino/Controller/EmployeeController.java
+++ src/main/java/com/time/trangecino/Controller/EmployeeController.java
@@ -1,7 +1,5 @@
 package com.time.trangecino.Controller;
 
-import com.time.trangecino.business.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(path = "/Employee")
 public class EmployeeController {
 
-    @Autowired
-    EmployeeService employeeService;
 
     @GetMapping
     public String getEmployeeView(){
